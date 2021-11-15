614d4be041acacd151c2e1a3c6940f4f53083e38
Paul <paul@technove.co>
2021-11-14 21:49:03 -0600

    Export key

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import "reflect-metadata";
 export * from "./container";
 export * from "./decorators/inject";
 export * from "./decorators/service";
+export * from "./key";
 export * from "./types";
