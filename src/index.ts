8487129e746892280311c7b1db8fee7399d18cbf
Paul <paul@technove.co>
2021-11-16 01:13:30 -0600

    Correctly export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import "reflect-metadata";
 export * from "./container";
 export * from "./decorators/inject";
 export * from "./decorators/service";
+export * from "./decorators/post-injection";
 export * from "./key";
 export * from "./types";
 export * from "./field";
